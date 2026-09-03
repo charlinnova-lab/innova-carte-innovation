9f4920cd2346960ec3402a2e33ae36da77276be6
charlinnova-lab <charlottepiau.innova@gmail.com>
2026-09-03 16:58:41 +0200

    Update generate_map_public.py

--- generate_map_public.py
+++ generate_map_public.py
@@ -270,7 +270,9 @@ INITIAL_ZOOM = 14
 m = folium.Map(
     location=[49.89, 2.30],
     zoom_start=INITIAL_ZOOM,
-    tiles="OpenStreetMap",
+#Carte type routard    tiles="OpenStreetMap",
+    tiles="https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}",
+    attr="Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ",
     max_zoom=19
 )
 
